6ad55053c5073d6c690397e9e2e6d39d002faede
Jaz Mani <jaz_mani@yahoo.ca>
2023-03-08 01:23:03 -0500

    Modified visitCounter.js to display the correct API Gateway value

--- visitCounter.js
+++ visitCounter.js
@@ -1,5 +1,5 @@
 
-fetch('https://xmt0mebg05.execute-api.us-east-2.amazonaws.com/default/UpdateVisitors', {
+fetch('https://xmt0mebg05.execute-api.us-east-2.amazonaws.com/default/api-gateway', {
     method: 'POST',
 })
 .then(response => response.json())
