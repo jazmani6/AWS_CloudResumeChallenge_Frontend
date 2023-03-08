bc3aa5109293dc9c7750487d6f3e6fec8a6c8254
Jaz Mani <jaz_mani@yahoo.ca>
2023-03-08 01:20:08 -0500

    Modified index.html, testing GitHub Actions workflow to update s3 bucket

--- visitCounter.js
+++ visitCounter.js
@@ -1,5 +1,5 @@
 
-fetch('https://a39k36sm6e.execute-api.us-east-2.amazonaws.com/default/UpdateVisitors', {
+fetch('https://xmt0mebg05.execute-api.us-east-2.amazonaws.com/default/UpdateVisitors', {
     method: 'POST',
 })
 .then(response => response.json())
