b6657b81c94e8300c418f527129278179b74d8d0
Jaz Mani <jaz_mani@yahoo.ca>
2023-03-07 22:47:47 -0500

    initial commit

---
+++ visitCounter.js
@@ -0,0 +1,13 @@
+
+fetch('https://a39k36sm6e.execute-api.us-east-2.amazonaws.com/default/UpdateVisitors', {
+    method: 'POST',
+})
+.then(response => response.json())
+.then(data => {
+  var dataInt = parseInt(data.Visitors);
+  document.getElementById("counter").innerHTML = 'You are visitor number:' + ' ' + dataInt;
+  console.log('You are visitor number:' + ' ' + dataInt);
+})
+.catch((error) => {
+    console.error('Error:', error);
+  });
